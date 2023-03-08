80267c7c7eec6fac35da86e6593b8c5bb47efa6e
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-03-08 09:14:19 +0200

    initial commit

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB2KV7nUgYVG7OEmEtn7hxHqesQtLjNoVg",
+    authDomain: "moivie-app.firebaseapp.com",
+    projectId: "moivie-app",
+    storageBucket: "moivie-app.appspot.com",
+    messagingSenderId: "114085669086",
+    appId: "1:114085669086:web:8de1ac5470d2ed1f0b7a4f"
+  };
