ddaea8aca46dea9d2d2a35ce8eb0b4691a670670
DavidJacobin <120710627+DavidJacobin@users.noreply.github.com>
2023-03-09 10:18:53 +0200

    setting up requests with axios

---
+++ src/middlewares/Request.js
@@ -0,0 +1,14 @@
+const API_KEY = '5af17e5c645b39fe70411f3c10427c70';
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&languege=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&languege=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&languege=en-USwith_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&languege=en-USwith_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&languege=en-USwith_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&languege=en-USwith_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&languege=en-USwith_genres=99`,
+};
+
+export default requests;
